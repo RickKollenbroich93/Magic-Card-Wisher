04afddf4c40a88a647237c46567d39fc97967bc3
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-06 10:49:25 +0200

    Add type i want to use and export function to create elements

---
+++ types.ts
@@ -0,0 +1,9 @@
+//---Type i want to use
+export type Card ={ 
+    name:string;//cardName
+    imageUrl:string;//Imglink
+    cmc:number;//ManaCosts
+    color:string[];//ColorOfCard
+    rarity:string;
+    set:string;
+}
